2e1f2269a3905918426b3628a9b3d843a5644fff
minkyzecapagods <138422838+minkyzecapagods@users.noreply.github.com>
2024-02-25 23:39:31 -0300

    Add files via upload

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer>
+        <p>Feito por Yasmin Giordano</p>
+        <p>Natal/RN - Brasil</p>
+        <p>&copy; 2024, minkyzecapagods.</p>
+    </footer>
+</template>
+  
+<style>
+    footer {
+        text-align: center;
+        background-color: #201658;
+        border-top: 2px solid #100b2e;
+        color: rgba(240, 248, 255, 0.5);
+        font-size: x-small;
+        padding:20px;
+        width: 100%;
+    }
+</style>
